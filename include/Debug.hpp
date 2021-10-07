2c52866bd02ef8f82f6fa9e12e5cea297dc7c048
wangxianyu <wangxianyu@lixiang.com>
2021-10-07 17:05:21 +0800

    add code

---
+++ include/Debug.hpp
@@ -0,0 +1,10 @@
+#ifndef OSQPEIGEN_DEBUG_HPP
+#define OSQPEIGEN_DEBUG_HPP
+#define OSQP_EIGEN_DEBUG_OUTPUT
+#include <iostream>
+
+namespace OsqpEigen {
+    std::ostream &debugStream();
+}
+
+#endif /* OSQPEIGEN_DEBUG_HPP */
---
+++ include/OsqpEigen.h
@@ -0,0 +1,16 @@
+/**
+ * @file OsqpEigen.h
+ * @author Giulio Romualdi, Stefano Dafarra
+ * @copyright  Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ * @date 2018
+ */
+#ifndef OSQPEIGEN_OSQPEIGEN_H
+#define OSQPEIGEN_OSQPEIGEN_H
+
+#include <Constants.hpp>
+#include <Data.hpp>
+#include <Settings.hpp>
+#include <Solver.hpp>
+#include <SparseMatrixHelper.hpp>
+
+#endif //OSQPEIGEN_OSQPEIGEN_H
